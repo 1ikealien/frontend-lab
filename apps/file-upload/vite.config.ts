0ef42ffb34be8836c168acfa58c2aaf24c996a5e
likealien <3269791918@qq.com>
2026-09-14 17:05:20 +0800

    feat(qiankun): 接入文件上传微应用

--- apps/file-upload/vite.config.ts
+++ apps/file-upload/vite.config.ts
@@ -1,7 +1,28 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import qiankunPlugin from 'vite-plugin-qiankun'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
+
+  plugins: [
+
+    vue(),
+
+    qiankunPlugin(
+      'file-upload',
+      {
+        useDevMode: true,
+      }
+    ),
+
+  ],
+
+  server: {
+    port: 3001,
+    cors: true,
+  },
+
+  base: './',
+
 })
