307ffb4c6b44f7ed92f617f58ec3adf5b45d5d2d
likealien <3269791918@qq.com>
2026-09-14 11:18:28 +0800

    feat(main): 完善TechCard页面

--- apps/main/src/views/TechLab.vue
+++ apps/main/src/views/TechLab.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import { techLabData } from '@/data/techLab'
+import TechCard from '@/components/techLab/TechCard.vue'
+</script>
 <template>
-  <div>TechLab</div>
+  <div class="techLab">
+    <TechCard
+      v-for="item in techLabData"
+      :key="item.name"
+      :name="item.name"
+      :description="item.description"
+      :techStack="item.techStack"
+      :demo="item.demo"
+      :features="item.features"
+    />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.techLab {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+}
+
+@media (max-width: 768px) {
+  .techLab {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
